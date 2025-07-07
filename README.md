6e5073456c1213f5bfaee5b5cc583490315e5eb7
phermiz <phermiz@umich.edu>
2025-07-06 20:45:44 -0500

    Added mutliple tickers to run

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # yieldmax-etfs
+
+## Pre-requisitites
+
+1. Python
+2. Pipenv
+
+## Setup and Installation
+
+First install the dependencies using `Pipenv`
+
+```bash
+pipenv install
+```
+
+Next, start up the virtual environment by running
+
+```bash
+pipenv shell
+```
+
+## Setup Environment Variables
+
+This script uses [Polygon.io](https://polygon.io/docs/rest/quickstart) REST API to fetch stock data. This requires you to have an API Key from Polygon. You can make a free account to get an API key. 
+
+In order to use this API Key you will have to create a `.env` file and set it up like so below:
+
+```.env
+API_KEY=<Your-API-KEY>
+```
+
+## Setup Distribution Detail Files
+
+In the directpory `src/resources` include the distribution details for your specified stock ticker in a `csv` file. For instance, if your stock ticker is `MSTY` create a file with the path `src/main/distribution-details-MSTY.csv` and grab the distribution details for that ETF from [yeildmaxetfs](https://www.yieldmaxetfs.com/) and paste it in there.
+
+## Running the 
+
+
+
--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
-import os
 from pathlib import Path
+from argparse import ArgumentParser
 
 
 from src.main.utils.csv_util import CSVUtil
@@ -9,32 +9,44 @@ from src.main.services.stock_calculator_service import StockCalculatorService
 
 def main() -> None:
     load_dotenv()  # Load environment variables from .env file
-
-    distribution_details_path = Path("src/resources/distribution-details.csv")
-    distribution_details = CSVUtil.load_distribtution_details(
-        distribution_details_path.resolve()
+    parser = ArgumentParser(description="Stock Calculator")
+    parser.add_argument(
+        "-t",
+        "--tickers",
+        nargs="+",
+        type=str,
+        required=True,
+        help="List of ticker symbols (e.g. MSTY AAPL TSLA)"
     )
+    args = parser.parse_args()
 
     open_market_dates_path = Path("src/resources/us_stock_market_open_days.csv")
     open_market_dates = CSVUtil.load_open_market_dates(open_market_dates_path.resolve())
 
-    stock_calculator_service = StockCalculatorService(
-        ticker="MSTY",
-        distributions=distribution_details,
-        market_open_dates=open_market_dates,
-    )
-
-    stock_calculations = []
-    for detail in distribution_details:
-        stock_calculation = stock_calculator_service.calculate_percentage_difference(
-            detail
+    for ticker in args.tickers:
+        print(f"Processing ticker: {ticker}")
+        distribution_details_path = Path(f"src/resources/distribution-details-{ticker}.csv")
+        distribution_details = CSVUtil.load_distribtution_details(
+            distribution_details_path.resolve()
         )
-        stock_calculations.append(stock_calculation)
 
-    CSVUtil.write_stock_calculations_to_csv(
-        file_path="src/resources/stock_calculations.csv",
-        stock_calculations=stock_calculations,
-    )
+        stock_calculator_service = StockCalculatorService(
+            ticker=ticker,
+            distributions=distribution_details,
+            market_open_dates=open_market_dates,
+        )
+
+        stock_calculations = []
+        for detail in distribution_details:
+            stock_calculation = stock_calculator_service.calculate_percentage_difference(
+                detail
+            )
+            stock_calculations.append(stock_calculation)
+
+        CSVUtil.write_stock_calculations_to_csv(
+            file_path=f"src/resources/stock_calculations_{ticker}.csv",
+            stock_calculations=stock_calculations,
+        )
 
 
 if __name__ == "__main__":
